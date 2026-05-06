c8bf503e6e33d96b2e6ed49b82e320e28ae7f64a
Andrei Ionita <andrei.ionita@adrem.ro>
2026-05-06 13:20:59 +0300

    Updating the model for Adrem

--- ml.py
+++ ml.py
@@ -5540,7 +5540,7 @@ def predicting_exporting_Adrem_15min(interval_from, interval_to, limitation_perc
 
 	df = df[["Data", "Interval", "Temperatura", "Nori", "Radiatie", "Dewpoint", "Umiditate"]]
 
-	xgb_loaded = joblib.load("./Adrem/rs_xgb_Adrem_prod_15min_0126.pkl")
+	xgb_loaded = joblib.load("./Adrem/rs_xgb_adrem_prod_15min_0326.pkl")
 
 	df["Month"] = df.Data.dt.month
 	IRR_COL = "Radiatie"      # <- adjust to your irradiance column name (e.g., "GHI")
