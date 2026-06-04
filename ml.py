5b0e5115ab09d99d26b70cbd5d7a9a511301e4cb
Andrei Ionita <andrei.ionita@adrem.ro>
2026-06-04 15:16:03 +0300

    Updating the model for MM&MV

--- ml.py
+++ ml.py
@@ -5263,7 +5263,7 @@ def predicting_exporting_MM_MV_15min(interval_from, interval_to, limitation_perc
 
 	df = df[["Data", "Interval", "Temperatura", "Nori", "Radiatie", "Dewpoint", "Umiditate"]]
 
-	xgb_loaded = joblib.load("./MM_MV/rs_xgb_mm_mv_prod_15min_0326.pkl")
+	xgb_loaded = joblib.load("./MM_MV/rs_xgb_mm&mv_prod_15min_0426.pkl")
 
 	df["Month"] = df.Data.dt.month
 	IRR_COL = "Radiatie"      # <- adjust to your irradiance column name (e.g., "GHI")
