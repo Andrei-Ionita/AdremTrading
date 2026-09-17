13c2875a872b915dc97cfdd351f18cd8e1480239
Andrei Ionita <andrei.ionita@adrem.ro>
2026-09-17 16:22:11 +0300

    Updating the model for Anto

--- ml.py
+++ ml.py
@@ -5849,7 +5849,7 @@ def predicting_exporting_Anto_15min(interval_from, interval_to, limitation_perce
 
 	df = df[["Data", "Interval", "Temperatura", "Nori", "Radiatie", "Dewpoint", "Umiditate"]]
 
-	xgb_loaded = joblib.load("./Anto/rs_xgb_anto_prod_15min_0726.pkl")
+	xgb_loaded = joblib.load("./Anto/rs_xgb_anto_prod_15min_0826.pkl")
 
 	df["Month"] = df.Data.dt.month
 	IRR_COL = "Radiatie"      # <- adjust to your irradiance column name (e.g., "GHI")
